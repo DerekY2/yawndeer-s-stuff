96a197efb480af7d522a790df048181f150748fe
DerekY2 <derek.yu869@gmail.com>
2024-12-19 02:19:56 -0500

    init yawndeer's stuff

---
+++ javascripts/popup.js
@@ -0,0 +1,17 @@
+
+
+document.querySelector('.dropbtn').addEventListener('click', function() {
+  document.querySelector('.dropdown-stuff').classList.toggle('show');
+});
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-stuff");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
