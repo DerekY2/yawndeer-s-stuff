872c43d21e74169f007dbb8d1c75d1e8774dd760
DerekY2 <derek.yu869@gmail.com>
2024-12-21 17:03:02 -0500

    reaction-time support

--- armory/reaction-time.js
+++ armory/reaction-time.js
@@ -3,11 +3,8 @@ if(document.querySelector('#root')){
     for(let mutation of mutations){
       if(mutation.type ==='childList'){
         console.log("Mutation detected...")
-        if(document.querySelector('.full-nav')){
-          button = document.querySelector('.full-nav')
-          console.log("About to click...",button)
-          button.click()
-          console.log("Sent click request")
+        if(document.querySelector('#root > div > div:nth-child(4) > div.view-go.e18o0sx0.css-saet2v.e19owgy77')){
+          simClick(document.querySelector('#root > div > div:nth-child(4) > div.view-go.e18o0sx0.css-saet2v.e19owgy77'))
         }
       }
     }
@@ -15,3 +12,28 @@ if(document.querySelector('#root')){
 
   observer.observe(document.querySelector('#root'), { childList: true, subtree: true });
 }
+
+// document.addEventListener('click', ()=>{
+//   document.querySelector('.full-nav').click()
+//   console.log("Clicked nav")
+// })
+
+function simClick(selector){
+  var box = selector.getBoundingClientRect(),
+  coordX = box.left + (box.right - box.left) / 2,
+  coordY = box.top + (box.bottom - box.top) / 2;
+  simulateMouseEvent (selector, "mousedown", coordX, coordY);
+  simulateMouseEvent (selector, "mouseup", coordX, coordY);
+  simulateMouseEvent (selector, "click", coordX, coordY);
+}
+
+function simulateMouseEvent(element, eventName, coordX, coordY) {
+  element.dispatchEvent(new MouseEvent(eventName, {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: coordX,
+    clientY: coordY,
+    button: 0
+  }));
+};
