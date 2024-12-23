00ff0ac294c337d14840ee6bf05f06dcd6d92a9e
DerekY2 <derek.yu869@gmail.com>
2024-12-23 04:16:31 -0500

    0.1.3c changes

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -32,7 +32,7 @@ nodeSelectors.forEach(selector =>{
 // listener - open updates.html when clicked
 let showUpdates = document.querySelector('.show-version-details')
 showUpdates.addEventListener('click',(e)=>{
-  open('updates.html')
+  open(showUpdates.dataset.url)
   window.close()
   console.log('opened updates.html')
 })
@@ -40,24 +40,17 @@ showUpdates.addEventListener('click',(e)=>{
 function inject(file, request_url){
   if(file){
   var targetTab
-    chrome.tabs.query({}, tabs => {
-      var tabList = []
-      tabs.forEach(tab =>{
-        if(tab.url===request_url){
-          tabList.push(tab)
-          console.log("Found ",request_url,"activated:",tab)
-        }
-      })
-      if(tabList.length>0){
-        chrome.tabs.update(tabList[tabList.length-1].id, {active: true})
-        targetTab = tabList[tabList.length-1].id
+    chrome.tabs.query({'url':request_url}, tabs => {
+      if(tabs && tabs.length>0){
+        chrome.tabs.update(tabs[tabs.length-1].id, {active: true})
+        targetTab = tabs[tabs.length-1].id
         chrome.scripting.executeScript({
           target: {tabId: targetTab},
           files: [file]
         })
       }
       else{
-        console.log("No matching tabs found - ",tabList)
+        console.log("No matching tabs found - ",tabs)
         chrome.tabs.create({url: request_url}, tab =>{
           console.log("new tab created")
           targetTab = tab.id
@@ -76,18 +69,12 @@ function inject(file, request_url){
 
 function open(request_url){
   if(request_url){
-    chrome.tabs.query({}, tabs => {
-      var tabList = []
-      tabs.forEach(tab =>{
-        if(tab.url.includes(request_url) || tab.url===chrome.runtime.getURL(request_url)){
-          tabList.push(tab)
-        }else{
-        }
-      })
-      if(tabList.length>0){
-        chrome.tabs.update(tabList[tabList.length-1].id, {active: true})
+    chrome.tabs.query({'url':request_url}, tabs => {
+      if(tabs.length>0){
+        chrome.tabs.update(tabs[tabs.length-1].id, {active: true})
       }
       else{
+        console.log("No matching tabs found - ",tabs)
         chrome.tabs.create({url: request_url})
       }
     })
