e2c8096947a3eed34b74fee3bd817f62375da31b
DerekY2 <derek.yu869@gmail.com>
2025-01-03 05:39:43 -0500

    0.2.1 Release Notes

--- index.js
+++ index.js
@@ -12,16 +12,24 @@ logo.addEventListener('click',()=>{
 
 function toggleIcon(){
   console.log('before:',logo.src)
-  if(logo.src.includes('images/Sparkle_Doll.png')){
+  if(logo.src.includes('images/sky-icon.png')){
     logo.src='images/pull-shark.png'
     favicon.setAttribute('href','images/pull-shark.png')
     changeIconBtn.classList.add('bx-color')
     changeIconBtn.classList.remove('bxs-color')
-  }else{
+    changeIconBtn.classList.remove('bxs-moon')
+  }else if(logo.src.includes('images/pull-shark.png')){
     logo.src='images/Sparkle_Doll.png'
     favicon.setAttribute('href','images/Sparkle_Doll.png')
     changeIconBtn.classList.add('bxs-color')
     changeIconBtn.classList.remove('bx-color')
+    changeIconBtn.classList.remove('bxs-moon')
+  }else if(logo.src.includes('images/Sparkle_Doll.png')){
+    logo.src='images/sky-icon.png'
+    favicon.setAttribute('href','images/sky-icon.png')
+    changeIconBtn.classList.add('bxs-moon')
+    changeIconBtn.classList.remove('bx-color')
+    changeIconBtn.classList.remove('bxs-color')
   }
   console.log('after:',logo.src)
 }
