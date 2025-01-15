a0857384581356ebf780ae1cf6a666de7fa171eb
DerekY2 <derek.yu869@gmail.com>
2025-01-14 22:22:21 -0500

    PHP server

---
+++ backend/handle_calendar.php
@@ -0,0 +1,27 @@
+<?php
+  include '../info/timetableinfo.inc';
+
+  $conn = new mysqli("Host IP", "User", "Auth", "Database"); // placeholder values for public release
+
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  $data = json_decode(file_get_contents('php://input'), true);
+  $name = $data['name'];
+  $date = $data['time'];
+  $school = $data['institution'];
+  $term = $data['term'];
+  $info = $data['info'];
+  $calendar = $data['calendar'];
+
+  if($school=='carleton'){
+    $stmt = $conn->prepare("INSERT INTO carleton(date, user, term, events, calendar_data) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $date, $name, $term, $info, $calendar);
+  }
+
+  $stmt->execute();
+
+  $stmt->close();
+  $conn->close();
+?>
